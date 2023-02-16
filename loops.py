b39a534c095e81900861813a4589e86bdd3982b4
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-16 22:09:01 +0100

    While loop car game - my solution.

--- loops.py
+++ loops.py
@@ -1,4 +1,5 @@
 # while loop guessing game
+'''
 secret_nr = 5
 guess_coumt = 0
 guess_limit = 3
@@ -10,3 +11,21 @@ while guess_coumt < guess_limit:
         break
 else:
     print("Not this time!")
+'''
+# car simulation game
+start_condition = input("Do you wanna play a car game? Y/N ")
+while start_condition.upper() == "Y":
+    action = input("Type:"
+          "\nstart - to start the car"
+          "\nstop - to stop the car"
+          "\nquit - to exit\n"
+    )
+    if action == "start":
+        print("The engine starts.")
+    elif action == "stop":
+        print("The engine stops.")
+    elif action == "quit":
+        break
+        # start_condition = False
+    else:
+        print("I can't execute this command.")
