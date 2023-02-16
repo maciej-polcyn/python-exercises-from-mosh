c3b617bae368019f28d7fadefc25e940ddf783b1
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-16 22:21:20 +0100

    While loop car game - optimized solution.

--- loops.py
+++ loops.py
@@ -13,19 +13,19 @@ else:
     print("Not this time!")
 '''
 # car simulation game
-start_condition = input("Do you wanna play a car game? Y/N ")
-while start_condition.upper() == "Y":
-    action = input("Type:"
-          "\nstart - to start the car"
-          "\nstop - to stop the car"
-          "\nquit - to exit\n"
-    )
-    if action == "start":
+command = ''
+while command != "quit":
+    command = input("> ").lower()
+    if command == "help":
+        print('''Type:
+        start - to start the car
+        stop - to stop the car
+        quit - to exit''')
+    elif command == "start":
         print("The engine starts.")
-    elif action == "stop":
+    elif command == "stop":
         print("The engine stops.")
-    elif action == "quit":
+    elif command == "quit":
         break
-        # start_condition = False
     else:
         print("I can't execute this command.")
