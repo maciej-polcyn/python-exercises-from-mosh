9a13d24bf4b6ccc5005742fb380882f8950e243b
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-16 20:12:05 +0100

    BMR calculator with formatted string

--- to-be-determined.py
+++ to-be-determined.py
@@ -1,3 +1,7 @@
-input('How does line breaking work? \n')
-print(10/3)
-print(10//3)
+gender = input ("What's your gender? M/K ")
+weight = input('How much do you weight in kg? ')
+height = input("what's your height in cm? ")
+age = input("How old are you? ")
+bmr = round(88.362 + (13.397 * int(weight)) + (4.799 * int(height)) - (5.677 * int(age)))
+
+print(f"Your Basal Metabolic Rate equals {bmr} kcal.")
