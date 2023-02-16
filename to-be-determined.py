3427d06db4a99c5a36f8d4ab15429b287b4c4f6b
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-16 20:20:21 +0100

    Adds a formula to calculate bmr for women

--- to-be-determined.py
+++ to-be-determined.py
@@ -2,6 +2,12 @@ gender = input ("What's your gender? M/K ")
 weight = input('How much do you weight in kg? ')
 height = input("what's your height in cm? ")
 age = input("How old are you? ")
-bmr = round(88.362 + (13.397 * int(weight)) + (4.799 * int(height)) - (5.677 * int(age)))
+
+
+if gender.upper() == 'M':
+    bmr = round(88.362 + (13.397 * int(weight)) + (4.799 * int(height)) - (5.677 * int(age)), 2)
+else:
+    bmr = round(447.593 + (9.247 * int(weight)) + (3.098 * int(height)) - (4.330 * int(age)), 2)
+
 
 print(f"Your Basal Metabolic Rate equals {bmr} kcal.")
