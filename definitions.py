7a990a583b17c6ee37e79687a3300dea9b613676
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-18 18:34:27 +0100

    Function with a parameter

---
+++ definitions.py
@@ -0,0 +1,13 @@
+def appreciate_effort():
+    print('You just defined your first function!')
+    print("You'll soon write amazing codes!")
+
+
+def greet_user(name):  # name is a parameter of the function
+    print(f'''
+Hi {name},
+Welcome aboard!
+    ''')
+
+
+greet_user('Maciej')  # 'Maciej' is an argument of the function
