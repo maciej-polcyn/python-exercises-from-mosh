00d250a975e101401d4b95998cb1bd598b67041e
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-18 18:18:26 +0100

    Emoji converter

--- dictionaries.py
+++ dictionaries.py
@@ -5,17 +5,28 @@
 # }
 # print(customer.get('verified')) # lub customer['verified']
 
-number_conversion = {
-    "1" : "One",
-    "2" : "Two",
-    "3" : "Three",
-    "4" : "Four",
-    "5" : "Five"
-}
+# number_conversion = {
+#     "1" : "One",
+#     "2" : "Two",
+#     "3" : "Three",
+#     "4" : "Four",
+#     "5" : "Five"
+# }
+#
+# phone = input("Enter your phone number: ")
+# phone_converted = ""
+# for i in phone:
+#     #phone_converted += number_conversion[i] + " "
+#     phone_converted += number_conversion.get(i) + " "
+# print(phone_converted)
 
-phone = input("Enter your phone number: ")
-phone_converted = ""
-for i in phone:
-    #phone_converted += number_conversion[i] + " "
-    phone_converted += number_conversion.get(i) + " "
-print(phone_converted)
+# emoji converter
+message = input('> ')
+words = message.split(' ')
+emojis = {
+    ":)": "😊"
+}
+output = ''
+for word in words:
+    output += emojis.get(word, word) + ' '
+print(output)
