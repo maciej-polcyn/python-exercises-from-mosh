ac604b27b3bb360685b35034908abfacc6afc913
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-19 19:17:37 +0100

    Emoji converter with as a defined function.

--- dictionaries.py
+++ dictionaries.py
@@ -30,3 +30,18 @@ output = ''
 for word in words:
     output += emojis.get(word, word) + ' '
 print(output)
+
+
+def emoji_converter(message):
+    words = message.split(" ")
+    emojis ={
+        ":)" : "😊",
+        ":(" : "😞"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+print(emoji_converter("Where did it all go? :("))
