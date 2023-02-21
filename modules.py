ca6ffa8d6f852cdef7a84cdab70c9a6cab1b6778
Maciej Polcyn <maciej.polcynn@gmail.com>
2023-02-21 23:45:34 +0100

    Creating a new package called shipping and different types of imports in modules file.

--- modules.py
+++ modules.py
@@ -1,4 +1,13 @@
+import ecommerce.shipping
 import utility_functions as uf
+# import ecommerce.shipping
+# from ecommerce import shipping
+from ecommerce.shipping import calc_shipping
+
 
 numbers = [23, 32, 12, 109, 51, 65, 12, 67, 38]
 print(uf.find_max(numbers))
+
+# ecommerce.shipping.calc_shipping()
+# shipping.calc_shipping()
+calc_shipping()
